572fb0dcd8e63e14f204c43b57e7c653913c3bfe
Felipe Ribeiro <felipeboy_157@hotmail.com>
2019-11-07 19:15:06 -0300

    Calculator fim

--- calculator/App.js
+++ calculator/App.js
@@ -3,10 +3,18 @@ import { StyleSheet, View, Text } from 'react-native';
 import Button from './src/component/Button'
 import Display from './src/component/Display'
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 export default class App extends Component {
 
   state = {
-    displayValue: '0'
+    ...initialState
   }
 
   render() {
@@ -30,7 +38,7 @@ export default class App extends Component {
           <Button label='+' operation onClick={() => this.setOperation('+')}/>
           <Button label='0' double onClick={this.addDigit} />
           <Button label='.' onClick={this.addDigit} />
-          <Button label='=' />
+          <Button label='=' operation onClick={() => this.setOperation('+')}/>
         </View>
       </View>
     )
@@ -38,35 +46,89 @@ export default class App extends Component {
 
   // Funções
   addDigit = n => {
-    if (n === '.' && this.state.displayValue.includes('.')) {
-      n = ''
+    //console.debug(typeof this.state.displayValue)
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+    
+    if (n === '.' && 
+        !clearDisplay && 
+        this.state.displayValue.includes('.')) {
+      return
     }
-    let junta = this.state.displayValue.concat(n)
-    if (junta[0] === '0' && junta[1] !== '.') {
-      junta = junta.substring(1, junta.length);
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
     }
-    this.setState({ displayValue: junta })
   }
 
+
   clearMemory = () => {
-    this.setState({ displayValue: '0'})
+    this.setState({ ...initialState })
   }
 
   setOperation = operation => {
-    const lista = Array(2)
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const isEquals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
 
-
-
-
-
-
-
-
-
-
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: isEquals ? null : operation,
+        current: isEquals ? 0: 1,
+        //clearDisplay: !isEquals,
+        clearDisplay: true,
+        values
+      })
+    }
   }
-}
 
+
+//  addDigit = n => {
+//     if (n === '.' && this.state.displayValue.includes('.')) {
+//       n = ''
+//     }
+//     let junta = this.state.displayValue.concat(n)
+//     if (junta[0] === '0' && junta[1] !== '.') {
+//       junta = junta.substring(1, junta.length);
+//     }
+//     this.setState({ displayValue: junta })
+//   }
+
+
+//   setOperation = operation => {
+//     if (operation === '+' && (this.state.displayValue.includes('+')|| 
+//                               this.state.displayValue.includes('-')|| 
+//                               this.state.displayValue.includes('*')||
+//                               this.state.displayValue.includes('/'))) {
+//       operation = ''
+//     }
+//     let lista = Array(2)
+//     let result = 0
+//     const operador1 = this.state.displayValue
+//     const operador2 = 0
+//     lista[0] = parseFloat(operador1)
+//     lista[1] = parseFloat(operador2)
+
+//     if (operation === '+') {
+//       result = operador1 + operador2
+//     }
+//   }
+// }
+}
 
 const styles = StyleSheet.create({
   container: {
