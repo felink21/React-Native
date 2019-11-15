2bff250a8ca542b4dca6b33ddb4d77d49c294e33
Felipe Ribeiro <felipeboy_157@hotmail.com>
2019-11-15 18:53:19 -0300

    implement field on app

--- cminado/App.js
+++ cminado/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 import params from './src/params'
-import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/functions'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount())
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -11,19 +32,9 @@ export default class App extends Component {
         <Text>
           Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
-
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged />
-        <Field flagged opened />
-        <Field flagged bigger />
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
       </View>
     )
   }
@@ -32,9 +43,11 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    //justifyContent: 'space-around',
+    justifyContent: 'flex-end',
+    //justifyContent: 'space-around'
+  },
+  board: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#AAA'
   }
 })
